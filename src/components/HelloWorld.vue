4bcf3d17b11c41a3a7d79bb30c17bbe68ea0e870
Vicenç Álvarez <vicens17@gmail.com>
2021-06-13 19:43:24 +0200

    Primeros pasos en el proyecto

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,64 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Star Wars Characters</h1>
+    <template v-if="showLinks">
+      <ul v-if="characters?.length > 0">
+        <li v-for="character of characters" :key="character.url"><a :href="character.url" target="_blank" rel="noopener">{{ character.name }}</a></li>
+      </ul>
+      <div v-else>No Characters</div>
+
+    </template>
+    <button @click="fetchStarWarsCharacter()">Fetch characters</button>
+
+    <Content>
+      <template #title>
+        <b>
+          titulo
+        </b>
+      </template>
+
+      <div>
+        hello
+      </div>
+    </Content>
+
   </div>
 </template>
 
 <script>
+import Content from './Content'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    Content
+  },
   props: {
-    msg: String
-  }
+    msg: String,
+    msg2: String,
+    showLinks: Boolean,
+  },
+  data() {
+    return {
+      characters: [],
+    }
+  },
+  computed: {
+    msg3() {
+      return this.msg.replace('h', 'H')
+    }
+  },
+  methods: {
+    async fetchStarWarsCharacter() {
+      const response = await fetch('http://swapi.dev/api/people/')
+      const responseJson = await response.json()
+      this.characters = responseJson.results
+    },
+  },
+  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
