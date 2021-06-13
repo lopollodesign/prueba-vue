6e9689e69fd959bb344a74a4fd4f3eba4dd4e568
Vicenç Álvarez <vicens17@gmail.com>
2021-06-13 19:46:26 +0200

    Archivos restantes

---
+++ src/components/Content.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <div>
+      <slot name="title"></slot>  
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+}
+</script>
+
+<style scoped>
+  .content {
+    padding: 20px;
+    background-color: #ddd;
+    color: #000;
+  }
+</style>
