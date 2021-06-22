64d8d64cbaa8cc444c12066d268089415e08a256
lopollodesign <vicens17@gmail.com>
2021-06-22 18:45:12 +0200

    Conecto con la API de coincap y hago un listado de criptos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,60 @@
 <template>
   <div class="home">
-    hola
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <table style="width: 100%">
+        <thead>
+          <th></th>
+          <th>rank</th>
+          <th>Name</th>
+          <th>Price Usd</th>
+          <th>Market Cap Usd</th>
+          <th>Change Percent 24Hr</th>
+        </thead>
+        <tbody style="font-size: 14px">
+          <tr v-for="coin in assets" :key="coin.id">
+            <td>
+              <img :src="`https://static.coincap.io/assets/icons/${coin.symbol.toLowerCase()}@2x.png`" :alt="coin.name" width="30">
+            </td>
+            <td>
+              <b>#{{ coin.rank }}</b>
+            </td>
+            <td>
+              <b>{{ coin.name }}</b>
+            </td>
+            <td>
+              <b>{{ coin.priceUsd }}</b>
+            </td>
+            <td>
+              <b>{{ coin.marketCapUsd }}</b>
+            </td>
+            <td>
+              <b>{{ coin.changePercent24Hr }}</b>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import api from '@/api';
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      assets: []
+    }
+  },
+  created() {
+    api.getAssets()
+        .then(assets => (this.assets = assets))
   }
 }
 </script>
