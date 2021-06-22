bae43a1eb16ec2b25416599b94239b36fcadca92
lopollodesign <vicens17@gmail.com>
2021-06-22 19:03:28 +0200

    Instalo el numeral para limiar los numeros largos de las monedas. Lo dejo desactivado porque no me lo pilla bien

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// import { dollarFilter } from './coinfilters'
+// createApp(App).filter('dollar', dollarFilter)
+
 createApp(App).use(store).use(router).mount('#app')
