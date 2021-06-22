ec5f07ff79494c288c8a51daeef213c47070722a
lopollodesign <vicens17@gmail.com>
2021-06-22 18:45:28 +0200

    Creo la pagina de Error, aun no funciona

---
+++ src/views/Error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="Error">
+    Error
+    <router-link to="/">Volver a la pagina principal</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+}
+</script>
