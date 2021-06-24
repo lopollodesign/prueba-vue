44ad064c0e3f49755eb75e431aa0cf23ed81f7eb
lopollodesign <vicens17@gmail.com>
2021-06-24 13:15:13 +0200

    Uso el chart para mostrar una grafica del historial de la moneda

--- src/views/Coin.vue
+++ src/views/Coin.vue
@@ -3,6 +3,22 @@
     <div v-if="asset.id" class="container">
       <h1>Coin detail: {{ asset.name }} ({{ asset.symbol }})</h1>
     </div>
+
+    <div>
+      precio: {{ asset.priceUsd }}
+    </div>
+    <div>
+      min: {{ min }}
+    </div>
+    <div>
+      max: {{ max }}
+    </div>
+    <div>
+      avg: {{ avg }}
+    </div>
+
+    <line-chart :colors="['orange']" :min="min" :max="max" :data="history.map(h => [h.date , parseFloat(h.priceUsd).toFixed(2)])"/>
+
   </div>
 </template>
 
@@ -14,18 +30,34 @@ export default {
 
   data () {
     return {
-      asset: {}
+      asset: {},
+      history: []
     }
   },
-
   created() {
     this.getCoin()
   },
-
+  computed: {
+    min() {
+      return Math.min(...this.history.map(h => parseFloat(h.priceUsd).toFixed(2)))
+    },
+    max() {
+      return Math.max(...this.history.map(h => parseFloat(h.priceUsd).toFixed(2)))
+    },
+    avg() {
+      return Math.abs(...this.history.map(h => parseFloat(h.priceUsd).toFixed(2)))
+    }
+  },
   methods: {
-    getCoin () {
+    getCoin() {
       const id = this.$route.params.id
-      api.getAsset(id).then(asset => this.asset = asset)
+
+      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
+          ([asset, history]) => {
+            this.asset = asset
+            this.history = history
+          }
+      )
     }
   }
 }
